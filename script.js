49727dc2d66e5cf76c52e870b4e430c5bf59b724
anshuldaksh2016 <anshuldaksh2016@gmail.com>
2020-12-02 13:09:27 +0530

    script done

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
-const Input = document.querySelector('textarea[name=area]');
+const textarea = document.querySelector('textarea[name=area]');
 const btn = document.querySelector('.button');
 const output = document.querySelector('.output');
 //queryselectors
+
+const serverUrl = "https://api.funtranslations.com/translate/groot.json";
+
+
+function urlHandler(add) {
+    return `${serverUrl}?text=${add}`;
+}
+
+
+function errorHandler(error) {
+    console.log(error);
+    alert("error");
+}
+
+
+function clickHandler() {
+
+    const inputText = textarea.value;
+
+    fetch(urlHandler(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            output.innerText = translatedText;
+        })
+        .catch(errorHandler)
+
+
+}
+
+
+
+
+btn.addEventListener("click", clickHandler);
